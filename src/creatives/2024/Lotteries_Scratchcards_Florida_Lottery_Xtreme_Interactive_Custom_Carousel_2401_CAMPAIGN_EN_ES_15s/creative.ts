d73d4deca1d778958055aeac1509e1a1fdb5e900
Oghel Joye <oghel.joye@dailymotion.com>
2024-01-23 16:10:03 +0100

    creative Object - Carousel format

--- src/creatives/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_ES_15s/creative.ts
+++ src/creatives/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_ES_15s/creative.ts
@@ -2,8 +2,8 @@ import { VPAIDVideoPlayer } from "@app";
 import { random12 } from "@/utils/helper";
 import { ImageDM } from "@/components/image";
 import { bounceIn, rotate } from "@/animations";
-import { CreativeHandler, CreativeProps } from "@/types";
 import { CarouselBasic } from "@/components/carouselBasic";
+import { Creative, CreativeProps } from "@/creative";
 
 const ALL_DATA = {
 	en: {
@@ -63,145 +63,149 @@ const shineCoordinates = [
 	],
 ];
 
-const creative: CreativeHandler = (
-	root: HTMLElement,
-	{ onClick }: CreativeProps
-) => {
-	const clickUrl = data.redirection;
+class MyCreative extends Creative {
+	constructor(root: HTMLElement, { onClick }: CreativeProps) {
+		super();
+		const clickUrl = data.redirection;
 
-	// BG
-	const bg = new ImageDM(
-		"bg-dm",
-		`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/elements.png`
-	);
-	root.appendChild(bg);
-	bg.addEventListener("click", () => onClick(clickUrl));
+		// BG
+		const bg = new ImageDM(
+			"bg-dm",
+			`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/elements.png`
+		);
+		root.appendChild(bg);
+		bg.addEventListener("click", () => onClick(clickUrl));
 
-	const cta = new ImageDM(
-		"cta-dm",
-		`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/cta.png`,
-		{
-			width: "24%",
-			height: "8%",
-			left: "45%",
-			bottom: "11%",
-			backgroundSize: "contain",
-			cursor: "pointer",
-			scale: "0",
-		}
-	);
-	root.appendChild(cta);
-	bg.addEventListener("click", () => onClick(clickUrl));
-	bounceIn(cta, 450, 400);
+		const cta = new ImageDM(
+			"cta-dm",
+			`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/cta.png`,
+			{
+				width: "24%",
+				height: "8%",
+				left: "45%",
+				bottom: "11%",
+				backgroundSize: "contain",
+				cursor: "pointer",
+				scale: "0",
+			}
+		);
+		root.appendChild(cta);
+		bg.addEventListener("click", () => onClick(clickUrl));
+		bounceIn(cta, 450, 400);
 
-	const fadeObjects = shineCoordinates.map((stars) =>
-		stars.map(({ left, bottom, width }, i) => {
-			const star = new ImageDM(
-				`star-${i}-id`,
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/shine.png`,
+		const fadeObjects = shineCoordinates.map((stars) =>
+			stars.map(({ left, bottom, width }, i) => {
+				const star = new ImageDM(
+					`star-${i}-id`,
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/shine.png`,
+					{
+						width: `${width * 3}%`,
+						height: "unset",
+						aspectRatio: "1 / 1",
+						left: `${left}%`,
+						bottom: `${bottom}%`,
+						opacity: "0",
+					}
+				);
+
+				rotate(star, random12(3000, 7500));
+
+				return star;
+			})
+		);
+
+		// CAROUSEL component
+		const carousel = new CarouselBasic(
+			{
+				id: "carousel-dm",
+				productUrls: [
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_0.png`,
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_1.png`,
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_2.png`,
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_3.png`,
+					`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_4.png`,
+				],
+				focusedElementWidth: 100,
+				focusedElementHeight: 100,
+				debug: false,
+				gap: 20,
+				onClick,
+				clickUrl,
+				fadeObjects,
+				autoPlay: true,
+			},
+			{ width: "34%", height: "83%", right: 0 }
+		);
+		root.appendChild(carousel);
+
+		fadeObjects.flat().forEach((star) => {
+			carousel.appendChild(star);
+			star.animate(
+				[
+					{ scale: 0 },
+					{ scale: 1 },
+					{ scale: 1 },
+					{ scale: 1 },
+					{ scale: 1 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+					{ scale: 0 },
+				],
 				{
-					width: `${width * 3}%`,
-					height: "unset",
-					aspectRatio: "1 / 1",
-					left: `${left}%`,
-					bottom: `${bottom}%`,
-					opacity: "0",
+					delay: random12(0, 1200),
+					duration: 1000, //random12(1000, 2000),
+					fill: "forwards",
+					easing: "linear",
+					iterations: Infinity,
 				}
 			);
-
-			rotate(star, random12(3000, 7500));
-
-			return star;
-		})
-	);
-
-	// CAROUSEL component
-	const carousel = new CarouselBasic(
-		{
-			id: "carousel-dm",
-			productUrls: [
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_0.png`,
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_1.png`,
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_2.png`,
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_3.png`,
-				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/product_4.png`,
-			],
-			focusedElementWidth: 100,
-			focusedElementHeight: 100,
-			debug: false,
-			gap: 20,
-			onClick,
-			clickUrl,
-			fadeObjects,
-			autoPlay: true,
-		},
-		{ width: "34%", height: "83%", right: 0 }
-	);
-	root.appendChild(carousel);
-
-	fadeObjects.flat().forEach((star) => {
-		carousel.appendChild(star);
-		star.animate(
-			[
-				{ scale: 0 },
-				{ scale: 1 },
-				{ scale: 1 },
-				{ scale: 1 },
-				{ scale: 1 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-				{ scale: 0 },
-			],
-			{
-				delay: random12(0, 1200),
-				duration: 1000, //random12(1000, 2000),
-				fill: "forwards",
-				easing: "linear",
-				iterations: Infinity,
-			}
-		);
-	});
-
-	["left", "right"].forEach((name, i) => {
-		const btn = new ImageDM(
-			`${name}-id`,
-			`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/arrow.png`,
-			{
-				width: "13%",
-				height: "unset",
-				aspectRatio: "1 / 1",
-				left: i === 0 ? "3%" : "unset",
-				right: i === 1 ? "3%" : "unset",
-				top: "43%",
-				userSelect: "none",
-				cursor: "pointer",
-				transform: i === 0 ? "unset" : "rotate(180deg)",
-			}
-		);
-		btn.addEventListener("click", (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-			carousel.stopAutoPlay();
-			carousel.moveIndexBy(i === 0 ? 1 : -1);
 		});
-		carousel.appendChild(btn);
-	});
-};
 
-window.getVPAIDAd = () =>
-	new VPAIDVideoPlayer(creative, {
-		low: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_low.mp4`,
-		mid: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_mid.mp4`,
-		high: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_high.mp4`,
-	});
+		["left", "right"].forEach((name, i) => {
+			const btn = new ImageDM(
+				`${name}-id`,
+				`https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/arrow.png`,
+				{
+					width: "13%",
+					height: "unset",
+					aspectRatio: "1 / 1",
+					left: i === 0 ? "3%" : "unset",
+					right: i === 1 ? "3%" : "unset",
+					top: "43%",
+					userSelect: "none",
+					cursor: "pointer",
+					transform: i === 0 ? "unset" : "rotate(180deg)",
+				}
+			);
+			btn.addEventListener("click", (e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				carousel.stopAutoPlay();
+				carousel.moveIndexBy(i === 0 ? 1 : -1);
+			});
+			carousel.appendChild(btn);
+		});
+	}
+
+	public getVideos() {
+		return {
+			low: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_low.mp4`,
+			mid: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_mid.mp4`,
+			high: `https://statics.dmcdn.net/d/PRODUCTION/2024/Lotteries_Scratchcards_Florida_Lottery_Xtreme_Interactive_Custom_Carousel_2401_CAMPAIGN_EN_15s/assets/${data.assetsUrl}/video_high.mp4`,
+		};
+	}
+}
+customElements.define("dm-creative", MyCreative);
+
+window.getVPAIDAd = () => new VPAIDVideoPlayer(MyCreative);

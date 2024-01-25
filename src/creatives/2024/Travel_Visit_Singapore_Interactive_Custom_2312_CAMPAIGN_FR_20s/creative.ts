f29ff1fd77aa19a69de7bded9d335239b333dd05
Oghel Joye <oghel.joye@dailymotion.com>
2024-01-25 17:44:24 +0100

    Singapore floodlights

--- src/creatives/2024/Travel_Visit_Singapore_Interactive_Custom_2312_CAMPAIGN_FR_20s/creative.ts
+++ src/creatives/2024/Travel_Visit_Singapore_Interactive_Custom_2312_CAMPAIGN_FR_20s/creative.ts
@@ -1,42 +1,77 @@
 import { VPAIDVideoPlayer } from "@app";
-import { random12 } from "@/utils/helper";
 import { fetchWeatherApi } from "openmeteo";
 import { createDiv } from "@/utils/divMaker";
 import { Creative, CreativeProps } from "@/creative";
+import { random12, trackPixel } from "@/utils/helper";
 
 const ALL_DATA = {
 	food: {
 		assetsUrl: "food/assets/3",
 		winIndex: 1,
 		redirection: "https://www.visitsingapore.com/dining-drinks-singapore/",
+		ctaFloodlight:
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod005;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		btnFloodlights: [
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod002;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod003;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod004;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		],
 	},
 	gardens: {
 		assetsUrl: "gardens/assets/2",
 		winIndex: 2,
 		redirection:
 			"https://www.visitsingapore.com/editorials/singapores-iconic-green-spaces/#nature-wildlife",
+		ctaFloodlight:
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod009;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		btnFloodlights: [
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod006;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod007;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod008;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		],
 	},
 	jewel: {
 		assetsUrl: "jewel/assets/2",
 		winIndex: 2,
 		redirection:
 			"https://www.visitsingapore.com/singapore-itineraries/1-day-guide-to-jewel-changi/",
+		ctaFloodlight:
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod001;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		btnFloodlights: [
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod000;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		],
 	},
 	masjid: {
 		assetsUrl: "masjid/assets/2",
 		winIndex: 1,
 		redirection:
 			"https://www.visitsingapore.com/see-do-singapore/culture-heritage/",
+		ctaFloodlight:
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00c;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		btnFloodlights: [
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00-;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00a;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00b;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		],
 	},
 	pulau: {
 		assetsUrl: "pulau/assets/1",
 		winIndex: 2,
 		redirection:
 			"https://www.visitsingapore.com/editorials/explore-singapores-islands/",
+		ctaFloodlight:
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00g;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		btnFloodlights: [
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00d;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00e;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			"https://ad.doubleclick.net/ddm/activity/src=14084726;type=invmedia;cat=opsod00f;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+		],
 	},
 };
 
-const data = ALL_DATA.pulau;
+const data = ALL_DATA.food;
 
 class MyCreative extends Creative {
 	constructor(root: HTMLElement, { onClick }: CreativeProps) {
@@ -141,6 +176,19 @@ class MyCreative extends Creative {
 			transition: "all .5s ease-out",
 		});
 
+		const cta = createDiv("cta-id", {
+			position: "absolute",
+			width: "28%",
+			height: "11%",
+			left: "36%",
+			bottom: "19%",
+			pointerEvents: "none",
+		});
+		cta.addEventListener("click", () => {
+			trackPixel(data.ctaFloodlight);
+			onClick(data.redirection);
+		});
+
 		const optElements = ["A", "B", "C"].map((choice) => ({
 			unselected: `https://statics.dmcdn.net/d/PRODUCTION/2024/Travel_Visit_Singapore_Food_Interactive_Custom_2312_CAMPAIGN_FR_20s/${data.assetsUrl}/${choice}_unselected.png`,
 			selected: `https://statics.dmcdn.net/d/PRODUCTION/2024/Travel_Visit_Singapore_Food_Interactive_Custom_2312_CAMPAIGN_FR_20s/${data.assetsUrl}/${choice}_selected.png`,
@@ -191,6 +239,7 @@ class MyCreative extends Creative {
 				button.addEventListener("click", (e: MouseEvent) => {
 					e.stopImmediatePropagation();
 					e.stopPropagation();
+					trackPixel(data.btnFloodlights[i]);
 					if (!isAnimPlayed) {
 						isAnimPlayed = true;
 						if (!isBtnClicked) {
@@ -217,6 +266,8 @@ class MyCreative extends Creative {
 								element.style.opacity = "0";
 								element.style.pointerEvents = "none";
 							});
+							cta.style.pointerEvents = "auto";
+							cta.style.cursor = "pointer";
 						}, 500);
 						setTimeout(
 							() => bg.appendChild(temperatureContainer),
@@ -241,6 +292,7 @@ class MyCreative extends Creative {
 		root.appendChild(bg);
 		root.appendChild(transitionBlockGreen);
 		root.appendChild(transitionBlockRed);
+		root.appendChild(cta);
 	}
 	public getVideos() {
 		return {

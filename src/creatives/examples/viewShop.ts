78745222a4b9dd44caa5dcb62295be2316915bc3
Oghel Joye <oghel.joye@dailymotion.com>
2024-02-08 16:05:35 +0100

    theumbnails in panel

--- src/creatives/examples/viewShop.ts
+++ src/creatives/examples/viewShop.ts
@@ -3,10 +3,12 @@ import { Creative, CreativeProps } from "@/creative";
 import { viewShopTemplate } from "@creatives/templates/ViewShop";
 
 class MyCreative extends Creative {
+	private viewShopTimeUpdate: (percentage: number) => void;
+
 	constructor(root: HTMLElement, creativeProps: CreativeProps) {
 		super();
 
-		viewShopTemplate(root, creativeProps, {
+		this.viewShopTimeUpdate = viewShopTemplate(root, creativeProps, {
 			clickUrl: "https://www.dailymotion.fr",
 			hotSpotUrl:
 				"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/hotspot.png",
@@ -14,9 +16,147 @@ class MyCreative extends Creative {
 				{
 					videoProgress: 8,
 					bgUrl: "https://plus.unsplash.com/premium_photo-1706896055883-880d1710dc2f?q=80&w=402&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://plus.unsplash.com/premium_photo-1706896055883-880d1710dc2f?q=80&w=402&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 					products: [
 						{
 							clickUrl: "https://www.google.fr?q=product0",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 12,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 13,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 14,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 15,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 21,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 23,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 32,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
+							productUrl:
+								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
+							spotLeft: "94%",
+							spotTop: "33%",
+							cardLeft: "66%",
+							cardTop: "16%",
+						},
+					],
+				},
+				{
+					videoProgress: 42,
+					bgUrl: "https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					thumbnailUrl:
+						"https://images.unsplash.com/photo-1707306984355-4388b7ad51f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+					products: [
+						{
+							clickUrl: "https://www.google.fr?q=product1",
 							productUrl:
 								"https://statics.dmcdn.net/d/TESTS/fwk/assets/templates/hotSpots/product0.png",
 							spotLeft: "94%",
@@ -30,6 +170,10 @@ class MyCreative extends Creative {
 			cardStyle: {},
 			hotspotStyle: {},
 		});
+	}
+
+	public videoTimeUpdate(completionPercent: number): void {
+		this.viewShopTimeUpdate(completionPercent);
 	}
 
 	public getVideos() {
--- src/creatives/templates/ViewShop.ts
+++ src/creatives/templates/ViewShop.ts
@@ -1,18 +1,19 @@
 import { CssType } from "@/types";
 import { CreativeProps } from "@/creative";
 import { ImageDM } from "@/components/image";
-import { HotSpotProduct, hotSpotsTemplate } from "./HotSpots";
 import { createDiv } from "@/utils/divMaker";
+import { HotSpotProduct, hotSpotsTemplate } from "./HotSpots";
 
 export type ViewShopType = (
 	root: HTMLElement,
 	creativeProps: CreativeProps,
 	props: ViewShopProps
-) => void;
+) => (percentage: number) => void;
 
 interface PageProps {
 	videoProgress: number; // video percentage progress when the card should be added to the screen
 	bgUrl: string;
+	thumbnailUrl: string;
 	products: HotSpotProduct[];
 }
 
@@ -32,6 +33,8 @@ const defaultProps: Required<ViewShopProps> = {
 		{
 			videoProgress: 8,
 			bgUrl: "https://plus.unsplash.com/premium_photo-1706896055883-880d1710dc2f?q=80&w=402&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			thumbnailUrl:
+				"https://plus.unsplash.com/premium_photo-1706896055883-880d1710dc2f?q=80&w=402&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 			products: [
 				{
 					clickUrl: "https://www.google.fr?q=product0",
@@ -54,6 +57,9 @@ export const viewShopTemplate: ViewShopType = (
 	creativeProps: CreativeProps,
 	hotSpotProps: ViewShopProps
 ) => {
+	let currPage: HTMLElement | undefined;
+	let inactivityTimeout: number;
+
 	const actualProps: Required<ViewShopProps> = {
 		...defaultProps,
 		...hotSpotProps,
@@ -68,12 +74,22 @@ export const viewShopTemplate: ViewShopType = (
 		transition: "left .6s",
 		backgroundColor: "crimson",
 	});
+	const qqq = createDiv("qqq", {
+		position: "relative",
+		width: "100%",
+		height: "200%",
+		outline: "2px solid crimson",
+	});
+	leftPanel.appendChild(qqq);
 
-	const { clickUrl, pagesProps, cardStyle, hotspotStyle, hotSpotUrl } =
-		actualProps;
+	const { clickUrl, pagesProps, hotSpotUrl } = actualProps;
 
-	const pages = pagesProps.map(({ videoProgress, bgUrl, products }, i) => {
-		const page = new ImageDM(`page_${i}`, "");
+	const pages = pagesProps.map(({ bgUrl, products }, i) => {
+		const page = new ImageDM(`page_${i}`, "", {
+			// pointerEvents: "none",
+			left: "-100%",
+			transition: "left .6s",
+		});
 		hotSpotsTemplate(page, creativeProps, {
 			bgUrl,
 			clickUrl,
@@ -84,5 +100,58 @@ export const viewShopTemplate: ViewShopType = (
 		return page;
 	});
 
+	// hide the current page if any and resume the video when the user does not interact for some time
+	root.addEventListener("pointermove", () => {
+		window.clearTimeout(inactivityTimeout);
+		inactivityTimeout = window.setTimeout(() => {
+			if (currPage) {
+				currPage.style.left = "-100%";
+				currPage = undefined;
+				creativeProps.resumeAd();
+			}
+		}, 3000);
+	});
+
+	const thumbnails = pagesProps.map(({ thumbnailUrl }, i) => {
+		const thumbnail = new ImageDM(`thumbnail_${i}`, thumbnailUrl, {
+			width: "90%",
+			height: "17%",
+			top: `${2 + (17 + 2) * i}%`,
+			left: "5%",
+			cursor: "pointer",
+			opacity: "0",
+			transition: "opacity .6s",
+			pointerEvents: "none",
+		});
+		thumbnail.addEventListener("click", (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			console.log("click on thumbnail: ", i);
+
+			const nextPage = pages[i];
+			if (currPage === nextPage) {
+				return;
+			}
+			if (currPage) {
+				currPage.style.left = "-100%";
+			}
+			nextPage.style.left = "0%";
+			currPage = nextPage;
+
+			creativeProps.pauseAd();
+		});
+		leftPanel.appendChild(thumbnail);
+		return thumbnail;
+	});
+
 	root.appendChild(leftPanel);
+
+	return (percentage: number) => {
+		actualProps.pagesProps.forEach(({ videoProgress }, i) => {
+			if (percentage > videoProgress) {
+				thumbnails[i].style.opacity = "1";
+				thumbnails[i].style.pointerEvents = "auto";
+			}
+		});
+	};
 };

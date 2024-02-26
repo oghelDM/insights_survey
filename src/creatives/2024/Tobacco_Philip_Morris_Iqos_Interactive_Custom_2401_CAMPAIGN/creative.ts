a422672b9276f72a5adcd97be94a3a07942827c0
Oghel Joye <oghel.joye@dailymotion.com>
2024-02-26 16:22:12 +0100

    first version renamed to CLASSIC

--- src/creatives/2024/Tobacco_Philip_Morris_Iqos_Interactive_Custom_2401_CAMPAIGN/creative.ts
+++ src/creatives/2024/Tobacco_Philip_Morris_Iqos_Interactive_Custom_2401_CAMPAIGN/creative.ts
@@ -5,33 +5,107 @@ import { ImageDM } from "@/components/image";
 import { Creative, CreativeProps } from "@/creative";
 
 const ALL_DATA = {
-	STANDARD: {
-		floodlightClientPlayYes:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913560;dc_trk_aid=574989012;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
-		floodlightClientNo:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913566;dc_trk_aid=574989018;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
-		floodlightYes:
-			"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00v;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
-		floodlightPlay:
-			"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00u;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
-		clickTag:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913572;dc_trk_aid=574989024;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+	CLASSIC: {
+		STANDARD: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913560;dc_trk_aid=574989012;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913566;dc_trk_aid=574989018;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00v;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00u;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913572;dc_trk_aid=574989024;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+		INTENDERS: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913620;dc_trk_aid=574989072;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913626;dc_trk_aid=574989078;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i011;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i010;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913632;dc_trk_aid=575061310;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
 	},
-	INTENDERS: {
-		floodlightClientPlayYes:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913620;dc_trk_aid=574989072;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
-		floodlightClientNo:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913626;dc_trk_aid=574989078;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
-		floodlightYes:
-			"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i011;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
-		floodlightPlay:
-			"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i010;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
-		clickTag:
-			"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913632;dc_trk_aid=575061310;dc_trk_cid=207545163;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+	EDUCATION: {
+		STANDARD: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913560;dc_trk_aid=574989012;dc_trk_cid=210237641;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00p;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00o;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913572;dc_trk_aid=574989024;dc_trk_cid=210237641;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+		INTENDERS: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913620;dc_trk_aid=574989072;dc_trk_cid=210237641;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00x;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00w;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913632;dc_trk_aid=575061310;dc_trk_cid=210237641;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+	},
+	LEADGEN: {
+		STANDARD: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913563;dc_trk_aid=574989015;dc_trk_cid=210237644;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00r;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00q;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913575;dc_trk_aid=574989027;dc_trk_cid=210237644;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+		INTENDERS: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913623;dc_trk_aid=574989075;dc_trk_cid=210237644;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00z;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00y;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913635;dc_trk_aid=575061313;dc_trk_cid=210237644;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+	},
+	PRICE: {
+		STANDARD: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913593;dc_trk_aid=574989045;dc_trk_cid=210103159;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00t;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00s;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913605;dc_trk_aid=574989057;dc_trk_cid=210103159;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
+		INTENDERS: {
+			floodlightClientPlayYes:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.383913653;dc_trk_aid=575061331;dc_trk_cid=210103159;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+			floodlightClientNo: "",
+			floodlightYes:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i01;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			floodlightPlay:
+				"https://ad.doubleclick.net/ddm/activity/src=13654546;type=invmedia;cat=pmi_i00_;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1?",
+			clickTag:
+				"https://ad.doubleclick.net/ddm/trackclk/N571601.159483DAILYMOTION/B31096705.384092686;dc_trk_aid=575061343;dc_trk_cid=210103159;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+		},
 	},
 };
 
-const DATA = ALL_DATA.STANDARD;
+const DATA = ALL_DATA.CLASSIC.INTENDERS;
 
 class MyCreative extends Creative {
 	root: HTMLElement;

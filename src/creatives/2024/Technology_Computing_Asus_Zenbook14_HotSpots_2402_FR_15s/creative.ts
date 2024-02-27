e5465f20e3ad81b41d1d927b231b314ee19e5bd8
Oghel Joye <oghel.joye@dailymotion.com>
2024-02-27 16:47:36 +0100

    Asus hotspots property fix

--- src/creatives/2024/Technology_Computing_Asus_Zenbook14_HotSpots_2402_FR_15s/creative.ts
+++ src/creatives/2024/Technology_Computing_Asus_Zenbook14_HotSpots_2402_FR_15s/creative.ts
@@ -19,8 +19,6 @@ class MyCreative extends Creative {
 				"https://statics.dmcdn.net/d/PRODUCTION/2024/Technology_Computing_Asus_Zenbook14_HotSpots_2402_FR_15s/assets/hotspot.png",
 			clickUrl,
 			bgUrl: "https://statics.dmcdn.net/d/PRODUCTION/2024/Technology_Computing_Asus_Zenbook14_HotSpots_2402_FR_15s/assets/bg.png",
-			hotSpotUrl:
-				"https://statics.dmcdn.net/d/PRODUCTION/2024/Technology_Computing_Asus_Zenbook14_HotSpots_2402_FR_15s/assets/hotspot.png",
 			products: [
 				{
 					productUrl:
